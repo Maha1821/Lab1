58ee3685866e380d66b15986e6973b4a3ce206db
Maha1821 <s202180230@kfupm.edu.sa>
2023-09-06 23:01:35 +0300

    Final version

--- Sorter.java
+++ Sorter.java
@@ -1,26 +1,6 @@
-
 public class Sorter {
-
-	public static void main(String[] args) {
-
-		//sorter
-		 int[] arr = {64, 25, 12, 22, 11};
-	        System.out.println("Original Array:");
-	        for (int num : arr) {
-	            System.out.print(num + " ");
-	        }
-
-	        selectionSort(arr);
-
-	        System.out.println("\nSorted Array:");
-	        for (int num : arr) {
-	            System.out.print(num + " ");
-	        }
-	        
-	        
-	}
 	
-	public static void selectionSort(int[] arr) {
+    public static void selectionSort(int[] arr) {
         int n = arr.length;
 
         for (int i = 0; i < n - 1; i++) {
@@ -30,11 +10,51 @@ public class Sorter {
                     minIndex = j;
                 }
             }
-            
+
             // Swap the found minimum element with the element at index i
             int temp = arr[minIndex];
             arr[minIndex] = arr[i];
             arr[i] = temp;
         }
+    }
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Move elements of arr[0..i-1] that are greater than key
+            // to one position ahead of their current position
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {64, 25, 12, 22, 11};
+        int[] arr2 = {38, 27, 43, 3, 9, 82, 10};
+
+        System.out.println("Original Array (Selection Sort):");
+        printArray(arr1);
+        selectionSort(arr1);
+        System.out.println("Sorted Array (Selection Sort):");
+        printArray(arr1);
+
+        System.out.println("\nOriginal Array (Insertion Sort):");
+        printArray(arr2);
+        insertionSort(arr2);
+        System.out.println("Sorted Array (Insertion Sort):");
+        printArray(arr2);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 }
