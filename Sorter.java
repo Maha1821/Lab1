05c772d9862e9f27aa6d19c450bb0b4389a03653
Maha1821 <s202180230@kfupm.edu.sa>
2023-09-06 20:04:34 +0300

    Added the Selection

--- Sorter.java
+++ Sorter.java
@@ -5,5 +5,22 @@ public class Sorter {
 		// TODO Auto-generated method stub
 
 	}
+	
+	public static void selectionSort(int[] arr) {
+        int n = arr.length;
 
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            
+            // Swap the found minimum element with the element at index i
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
 }
